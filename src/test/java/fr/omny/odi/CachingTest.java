9918d705639b9b683540e9f3bef076579b576456
Fabien CAYRE <fabiencayre81@gmail.com>
2023-02-25 18:47:51 +0100

    Cache tests: use injector classes and proxies

--- src/test/java/fr/omny/odi/CachingTest.java
+++ src/test/java/fr/omny/odi/CachingTest.java
@@ -1,47 +1,54 @@
 package fr.omny.odi;
 
-
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import fr.omny.odi.caching.CacheProxyListener;
 import fr.omny.odi.caching.Caching;
-import fr.omny.odi.proxy.ProxyFactory;
 
 public class CachingTest {
 
-	@Test
-	public void test_Cache_Key() throws Exception {
-		var service = ProxyFactory.newProxyInstance(Service.class, List.of(new CacheProxyListener()));
-		long start = System.currentTimeMillis();
-		service.data("key");
-		long end = System.currentTimeMillis();
-		assertEquals(500L, end - start, 50L);
-		start = System.currentTimeMillis();
-		service.data("key");
-		end = System.currentTimeMillis();
-		assertEquals(0, end - start, 50L);
-		start = System.currentTimeMillis();
-		service.data("key2");
-		end = System.currentTimeMillis();
-		assertEquals(500L, end - start, 50L);
-	}
+  @Before
+  public void before() {
+    Injector.startTest();
+  }
 
-	public static class Service {
+  @After
+  public void after() {
+    Injector.wipeTest();
+  }
 
-		@Caching()
-		public String data(String s) {
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return "Hello world";
-		}
+  @Test
+  public void test_Proxy_Caching() throws Exception {
+    Injector.addSpecial(Service.class);
+    var service = Injector.getService(Service.class);
+    long start = System.currentTimeMillis();
+    service.data("key");
+    long end = System.currentTimeMillis();
+    assertEquals(500L, end - start, 50L);
+    start = System.currentTimeMillis();
+    service.data("key");
+    end = System.currentTimeMillis();
+    assertEquals(0, end - start, 50L);
+    start = System.currentTimeMillis();
+    service.data("key2");
+    end = System.currentTimeMillis();
+    assertEquals(500L, end - start, 50L);
+  }
 
-	}
+  @Component
+  public static class Service {
 
+    @Caching()
+    public String data(String s) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return "Hello world";
+    }
+  }
 }
